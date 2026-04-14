40608055ce30193433a3689a9f1371ff241b94d3
Mutiso Kioko <kiokoandy01@gmail.com>
2026-04-14 22:05:37 +0300

    Messed a little, I'm Starting afresh

--- script.js
+++ script.js
@@ -2,5 +2,6 @@ const menuToggle = document.querySelector('.menu-toggle');
 const navLinks = document.querySelector('.nav-links');
 
 menuToggle.addEventListener('click', () => {
+    console.log('Menu toggle clicked');
     navLinks.classList.toggle('active');
 });
